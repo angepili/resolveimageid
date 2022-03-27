f05f84bc6cc03351a4f0b146f0ec93f35f7260cd
angelo <angepili@icloud.com>
2022-03-27 22:09:55 +0200

    feat: create stream file

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import csv from "csv-parser";
 import * as fs from 'fs';
 import path, { resolve } from "path";
 
-const DIR = 'products';
+const SRC_DIR = './products';
+const DEST_DIRT = './export';
+const BASE_URL = 'https://stage-wpf.poliform.dev/?p=';
+const LANG = 'fr';
+const DEST_FILE = `${DEST_DIRT}/export-${LANG}.csv`;
 
 const getIdFromCsv = async lang  => {
 
   const __dirname = path.resolve();
-  const directoryPath = path.join(__dirname, DIR);
+  const directoryPath = path.join(__dirname, SRC_DIR);
 
   const results = [];
 
@@ -22,7 +26,7 @@ const getIdFromCsv = async lang  => {
       });
 
       if (file) {
-        fs.createReadStream( `${DIR}/${file}` )
+        fs.createReadStream( `${SRC_DIR}/${file}` )
         .pipe(csv())
         .on('data', (data) => {
           results.push( parseInt( data.ID ) )
@@ -36,19 +40,66 @@ const getIdFromCsv = async lang  => {
 
 };
 
+const writeFile = newLine => {
+  fs.appendFileSync( DEST_FILE , newLine, function (err) {
+    if (err) throw err;
+    console.log("It's saved!");
+  });
+}
+
+const initFile = () => {
+
+  fs.stat( DEST_FILE , function (err, stats) {
+ 
+    if (err)  return console.error(err);
+ 
+    fs.unlink( DEST_FILE  ,function(err){
+      if(err) return console.log(err);
+    });
+
+    fs.open( DEST_FILE , 'w', (err, file) => {
+      if (err) throw err;
+      console.log( `Create : ${DEST_FILE}` );
+    });
+
+ });
+
+}
+
 ( async () => {
 
   const browser = await puppeteer.launch({dumpio: false});
   const [ page ] = await browser.pages();
-  const list_id = await getIdFromCsv('fr');
-  
+  const list_id = await getIdFromCsv( LANG );
+
+  initFile();
   
   for (let i = 0; i < list_id.length; i++) {
-    const url = `https://stage-wpf.poliform.dev/?p=${list_id[i]}`;
+    const page_id = list_id[i];
+    const url = `${BASE_URL}${page_id}`;
+
+    console.log('------------------------------------------');
+
     try {
+
       await page.goto( url );
-      const data = await page.$$eval('img[data-image_id]', item => item.map( img => img.dataset.image_id ) )
-      console.table( data );
+
+      const listPageId = await page.$$eval('img[data-image_id]', item => {
+        return item.map( img => {
+          const { image_id } = img.dataset;
+          return parseInt( image_id ) 
+        })
+      });
+
+      const listPageIdLine = listPageId.join(',\n');
+      if( !listPageIdLine ) return;
+
+      const firstLine = i > 0 ? "\n" : "";
+      writeFile( firstLine + listPageIdLine );
+      console.log(`Adding product ${page_id}`);
+      console.table( listPageId );
+      console.info(` Added ${i+1} product of ${list_id.length} `)
+     
     }
     catch (err) {
       console.log( err )
